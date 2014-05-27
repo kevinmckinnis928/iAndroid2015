a0c5e3af5efa3bfa5759d2ead2348d838dc23997
ithurlow <ian_thurlow@att.net>
2014-05-26 23:30:04 -0700

    Cleaned up the code and moved everything out of the Lada class and into the RobotHelper class.

--- src/org/wintrisstech/erik/iaroc/Lada.java
+++ src/org/wintrisstech/erik/iaroc/Lada.java
@@ -3,9 +3,6 @@ package org.wintrisstech.erik.iaroc;
 import ioio.lib.api.IOIO;
 import ioio.lib.api.exception.ConnectionLostException;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.wintrisstech.sensors.UltraSonicSensors;
 
 import android.os.SystemClock;
@@ -21,12 +18,8 @@ public class Lada extends IRobotCreateAdapter {
 	public UltraSonicSensors sonar;
 	private boolean firstPass = true;;
 	private int commandAzimuth;
-	private int initialHeading;
-	private static final int FASTWHEEL = 200;
-	private static final int SLOWWHEEL = FASTWHEEL - 10;
-	private int counter = 0;
-	private List<Integer> averageCompassReading = new ArrayList<Integer>();
-	private float averageReading = 50;
+	//private int initialHeading;
+	private RobotHelper api;
 
 	/**
 	 * Constructs a Lada, an amazing machine!
@@ -42,76 +35,63 @@ public class Lada extends IRobotCreateAdapter {
 	 */
 	public Lada(IOIO ioio, IRobotCreateInterface create, Dashboard dashboard)
 			throws ConnectionLostException {
+		
 		super(create);
+		
 		sonar = new UltraSonicSensors(ioio);
+		
 		this.dashboard = dashboard;
+		
+		this.api = new RobotHelper(create);
 	}
 
 	public void initialize() throws ConnectionLostException {
+		
 		dashboard.log("iAndroid2014 happy version 140509A");
-		 initialHeading = readCompass();
-		 //driveDirect(100, 100);
+		
+		//initialHeading = readCompass();
 	}
-	public void averageList(){
-		for (int i = 0; i < 5; i++) {
-			averageReading += averageCompassReading.get(i);
-		}
-		averageReading = averageReading/5;
-	}
-
 	
 	/**
 	 * This method is called repeatedly
 	 * 
 	 * @throws ConnectionLostException
 	 */
+	
 	public void loop() throws ConnectionLostException {
 		
 		SystemClock.sleep(100);
+		
 		int compassReading = readCompass();
+		
 		dashboard.log(compassReading + "");
-		if (compassReading == averageReading) {
-			driveDirect(FASTWHEEL, FASTWHEEL);
+		
+		if(api.getCounter() < RobotHelperConstants.NUMBER_OF_READINGS_TO_AVERAGE) {
+			
+			dashboard.log("counter: " + api.getCounter());
+			
+			api.addAverageCompassReading(readCompass());
+			
+			api.incrementCounter();
 		}
-		else if (compassReading > averageReading) {
-			driveDirect(FASTWHEEL, SLOWWHEEL);
+		
+		if(api.getCounter() == RobotHelperConstants.NUMBER_OF_READINGS_TO_AVERAGE) {
+			
+			api.averageList();
+			
+			api.incrementCounter();
+		} else {
+			
+			api.raceInAStraightLine(readCompass());
 		}
-		else if (compassReading < averageReading) {
-			driveDirect(SLOWWHEEL, FASTWHEEL);
-		}
-		/*if(counter < 5) {
-			dashboard.log("counter: " + counter);
-			averageCompassReading.add(readCompass());
-			counter++;
-		}
-		if(counter == 5) {
-			//averageList();
-			raceInAStraightLine(readCompass());
-		}
-		*/
+		
 		
 		
 	}
 
-	private void raceInAStraightLine(int compassReading) throws ConnectionLostException {
-		if (compassReading == averageReading) {
-			driveDirect(FASTWHEEL, FASTWHEEL);
-		}
-		else if (compassReading > averageReading) {
-			driveDirect(FASTWHEEL, SLOWWHEEL);
-		}
-		else if (compassReading < averageReading) {
-			driveDirect(SLOWWHEEL, FASTWHEEL);
-		}
-	}
 
-	public void turn(int commandAngle) throws ConnectionLostException // Doesn't
-																		// work
-																		// for
-																		// turns
-																		// through
-																		// 360
-	{
+	public void turn(int commandAngle) throws ConnectionLostException {
+	// Doesn't work for turns through 360
 		int startAzimuth = 0;
 		if (firstPass) {
 			startAzimuth += readCompass();
---
+++ src/org/wintrisstech/erik/iaroc/RobotHelperConstants.java
@@ -0,0 +1,8 @@
+package org.wintrisstech.erik.iaroc;
+
+public final class RobotHelperConstants {
+	
+	public static final int NUMBER_OF_READINGS_TO_AVERAGE = 5;
+	public static final int FASTWHEEL = 200;
+	public static final int SLOWWHEEL = FASTWHEEL - 10;
+}
